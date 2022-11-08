84ae8c4e5f55350d9476c00fa44a2315daea2118
Vainottu <94010466+Vainottu@users.noreply.github.com>
2022-11-08 13:14:03 +0200

    Add files via upload

---
+++ Harjoitus 19 VB kääntö/Program.cs
@@ -0,0 +1,33 @@
+﻿public class ohjelma
+{
+
+    public static void Main()
+    {
+        decimal luku1, luku2, luku3, tulos;
+
+        Console.WriteLine("Anna ensimmäinen luku ");
+        luku1 = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine("Anna toinen luku ");
+        luku2 = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine("Anna vähennettävä luku ");
+        luku3 = Convert.ToInt16(Console.ReadLine());
+
+        tulos = luku1 + luku2 - luku3;
+        Console.WriteLine(tulos);
+
+
+
+
+        if (luku1 > luku2)
+        {
+            Console.WriteLine("Suurin annettu luku oli: " + luku1); 
+        }
+        else if (luku1 < luku2)
+        Console.WriteLine("Suurin annettu luku oli: " + luku2);
+    }
+
+
+
+
+
+    }
---
+++ pullonpalautus/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata.Ecma335;
+
+public class Pullonpalautus
+{
+
+
+
+     static void Main()
+
+
+    {
+        
+        int tölkit;
+        Console.Write("Montako tölkkiä palautat? ");
+        tölkit = Convert.ToInt32(Console.ReadLine());
+
+        int pienetpullot;
+
+        Console.Write("Montako pientä pulloa palautat? ");
+        pienetpullot = Convert.ToInt32(Console.ReadLine());
+
+        int isotpullot;
+        Console.WriteLine("Montako isoa pulloa palautat? ");
+        isotpullot = Convert.ToInt32(Console.ReadLine());
+
+        double hinta = Convert.ToDouble((tölkit * 0.15) + (pienetpullot * 0.20) + (isotpullot * 0.40));
+
+        Console.WriteLine("Palautuksesi summa on: " + hinta);
+
+    }
+
+
+    
+
+}
+
+
