6c10fc1e15ee5475aa77fb823ac4a4800219c350
Vainottu <94010466+Vainottu@users.noreply.github.com>
2022-11-08 13:20:47 +0200

    Add files via upload

---
+++ c#/Harjoitus 4/Program.cs
@@ -0,0 +1,41 @@
+﻿//Käytä try-catch -rakennetta virheestä toipumiseen siten, että alkuperäinen ohjelmakohta tulee kuitenkin toteutettua.
+//Ts. esimerkissä käyttäjältä odotetaan kokonaislukua.
+//Virheen jälkeen ei voida vain edetä ohjelmassa, vaan haluttu luku on saatava käyttäjältä
+
+using System;
+
+public class Ohjelma
+{
+    public static void Main()
+
+    {
+        int joku;
+        bool oikein;
+        oikein = false;
+        //do while kunnes arvo on true, voidaan jatkaa ohjelmaa
+        do
+        {
+            //pyydetään kokonaislukua
+            Console.Write(" Anna kokonaisluku: ");
+            
+            try
+            {
+                joku = Convert.ToInt16(Console.ReadLine());
+                oikein = true;
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Virheilmoitus: " + e.Message);
+            }
+            // Ohjelma voi jatkua
+            
+        }
+        while (oikein != true);
+
+        
+
+
+
+        }
+    } 
