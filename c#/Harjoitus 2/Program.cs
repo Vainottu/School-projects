501e8cae7eb31e48338af239f6a5199e3be7624f
Vainottu <94010466+Vainottu@users.noreply.github.com>
2022-11-08 13:19:42 +0200

    Add files via upload

---
+++ c#/Harjoitus 2/Program.cs
@@ -0,0 +1,81 @@
+﻿//Tee esimerkit while ja do-while -raklenteista. Pohjana voi käyttää aikaisemmin tehtyjä harjoituksia.
+//Tee esimerkki myös taulukkomuuttujan
+//määrittelystä ja käytöstä
+//Kun valmiit, kopioi ohjelmaosuus Word-dokumentti (kaikki samaan) ja lähetä.
+
+
+
+
+
+
+using Microsoft.VisualBasic;
+
+public class hyviätehtäviä
+
+{
+
+    public static void Main()
+    {
+        //Annetaan alkuarvo i:lle 0
+        int i = 0;
+       //aloitetaan while loop. 
+       //toistetaan kunnes ollaan suoritettu 4 kierros ja poistutaan
+        while (i < 4)
+        {
+            //Tulostetaan tekstiä
+            Console.WriteLine("Oppiminen on kivaa, jee!");
+            //Kasvatetaan in arvoa yhdellä ja aloitetaan kierros uudestaan
+            i++;
+        }
+
+
+        //alustetaan x arvolla 0
+        int x = 0;
+        //silmukka
+        do
+        {
+            //tulostetaan tekstiä niin on
+            Console.WriteLine("Niin on");
+            //kasvatetaan x arvoa yhdellä
+            x++;
+        }
+        //katotaan mikä on x arvo, ja alotetaan alusta jos arvo ei ole neljä. Poistutaan kun ollaan tulostettu 4krt
+        while (x < 4);
+
+
+
+
+        //Taulukkomuuttuja
+
+
+        System.Console.WriteLine("Anna 5 nimeä, välilöynneillä eroteltuna");
+        string[] taulukko = new string[5];
+
+        for (int j = 0; j < 5; j++)
+        {
+            taulukko[j] = System.Console.ReadLine(); //Lukee rivin ja tallentaa sen taulukkoon
+        }
+
+       
+        //Käydään taulukko läpi, siihen asti montako nimeä annettiin.
+        foreach (string item in taulukko)
+        {
+            System.Console.WriteLine(item);  // Tulostaa jokaisen laatikon
+        }
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ c#/Harjoitus 3/Ohjelma.cs
@@ -0,0 +1,41 @@
+﻿
+//Jatka edellistä ”Harjoituksia’”-kohtaa. Tee esimerkkejä kaikista. Käännä ensimmäisi harjoituksia toiselle kielelle.
+
+//Tee ohjelma, joka kirjoittaa annetun merkkijonon käänteisessä järjestyksessä
+//(esim. annettu ”sana” -> tuloksena ”anas”) Toteuta ohjelma ilman valmisfunktioita,
+//poislukien Len-funktio. Tee käyttöliittymään kaksi tekstilaatikkoa, joissa merkkijonot näkyvät
+
+
+using System.Runtime.InteropServices;
+
+public class tehtava
+{
+    public static void Main()
+
+    {
+        //Määritellään talukkoa
+        string[] taulukko = new string[5];
+
+        //Kysytään jotain sanaa 
+        Console.WriteLine("Anna viisi  sanaa");
+
+        //taulukko käydään läpi ja luetaan rivit
+        for (int i = 0; i < taulukko.Length; i++)
+        {
+            taulukko[i] = Console.ReadLine();
+        }
+    
+        Array.Reverse(taulukko);
+
+        foreach (string item in taulukko)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
+
+
+
+
+
+
